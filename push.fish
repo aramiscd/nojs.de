4238e516420d60ba73dd2e2144349ac514188c8c
Aramís Concepción Durán <aramis@systemli.org>
2023-06-08 10:23:20 +0200

    update

--- push.fish
+++ push.fish
@@ -2,6 +2,6 @@
 hugo -d docs
 git add --all
 git commit --message=update
-git -f push git@git.eee.gg:aramis/nojs.de
-git -f push git@git.sr.ht:~aramis/nojs.de
+git push -f git@git.eee.gg:aramis/nojs.de
+git push -f git@git.sr.ht:~aramis/nojs.de
 git push git@github.com:aramiscd/nojs.de
